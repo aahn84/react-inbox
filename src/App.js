02513daba6b1b666b96a6a303a25a53411808c59
Angela Ahn <29741357+aahn84@users.noreply.github.com>
2018-03-12 21:16:19 -0700

    add constructor

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: this.props.messages
+    }
+  }
+
   render() {
     return (
       <div className="App">
